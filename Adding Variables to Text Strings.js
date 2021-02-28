efec73530ad52dc90d10c12479d3f2ee933c3885
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-28 23:38:46 +0800

    Committed 2021/2/28

---
+++ Adding Variables to Text Strings.js
@@ -0,0 +1,5 @@
+// <script type = "text/javascript">
+// Need not to introduce the JavaScript tag in a JavaScript type file.
+    var mycar = "Corvette";
+    document.write("I like driving my " + mycar);
+// </script>
