a0a0b48d259e82ef6b867555aed5878263121a33
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-07 20:46:23 +0800

    Committed 2021/3/7

--- ch3_code.js
+++ ch3_code.js
@@ -7,4 +7,12 @@ var endeffect = "</strong>";
 var beginpara = "<p>";
 var endpara = "</p>";
 
-document.write(headingtext);
+document.write(headingtext);  // This writes the heading at the top of the page.
+
+// Adding the begineffect and endeffect variables to the left and right of the myintro variable
+// writes the introductory text in bold under the heading.
+document.write(begineffect + myintro + endeffect);
+
+// A new paragraph with a link, and then another new paragraph with the red text message.
+document.write(beginpara + linktag + endpara);
+document.write(beginpara + redtext + endpara);
