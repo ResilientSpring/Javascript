eae221001a57167713bd82941d8d5cb43194b0a5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-07 21:04:06 +0800

    Committed 2021/3/7

---
+++ ch3_code2.js
@@ -0,0 +1,18 @@
+var headingtext = "<h1>A Page of JavaScript</h1>";
+var myintro = "Hello, welcome to my JavaScript page!";
+var linktag = "<a href=\"http://www.scripttheweb.com\">Link to a Site</a>";
+var redtext = "<span style='color: darkred'>I am so colorful today!</span>";
+var begineffect = "<em>";
+var endeffect = "</em>";
+var beginpara = "<p>";
+var endpara = "</p>";
+
+document.write(headingtext);  // This writes the heading at the top of the page.
+
+// Adding the begineffect and endeffect variables to the left and right of the myintro variable
+// writes the introductory text in bold under the heading.
+document.write(begineffect + myintro + endeffect);
+
+// A new paragraph with a link, and then another new paragraph with the red text message.
+document.write(beginpara + linktag + endpara);
+document.write(beginpara + redtext + endpara);
