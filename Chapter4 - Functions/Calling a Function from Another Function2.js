8dd1caa64b01320e6eb2677b159dbdec2a1829fa
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-05 22:53:11 +0800

    Committed 2021/4/05

---
+++ Chapter4 - Functions/Calling a Function from Another Function2.js
@@ -0,0 +1,19 @@
+function update_alert() {
+    window.alert("Welcome! This site is updated daily!");
+}
+
+function section_alert() {
+    window.alert("Please visit the picture section!");
+}
+
+function links_alert() {
+    window.alert("Also, check out my links page!");
+}
+
+function get_messages() {
+    update_alert();
+    section_alert();
+    links_alert();
+}
+
+get_messages();
