5495ee9216da733c1e0708edcc8ede4383ce8188
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-28 22:27:35 +0800

    Committed 2021/06/28

---
+++ Chapter4 - Functions/Calling functions with arguments - Using local variables.js
@@ -0,0 +1,13 @@
+var mycar = "Honda";
+var paycheck = 1200;
+
+function new_car(){
+    var mycar = "Ferrari";
+    var paycheck = "3500";
+
+    window.alert("You make $" + paycheck + " and have a " + mycar);
+}
+
+new_car();
+window.alert("You make $" + paycheck + " and have a " + mycar);
+
