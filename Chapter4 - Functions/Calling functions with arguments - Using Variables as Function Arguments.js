a513da11b1ac11c2b7149fbb587d3bb9f6ed808f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-24 18:14:24 +0800

    Committed 2021/07/24

--- Chapter4 - Functions/Calling functions with arguments - Using Variables as Function Arguments.js
+++ Chapter4 - Functions/Calling functions with arguments - Using Variables as Function Arguments.js
@@ -4,3 +4,18 @@ function check_alert(pcheck){
 
 var paycheck = 1200;
 check_alert(paycheck);
+
+function check_alert2(pcheck, car){
+    window.alert("You make $ " + pcheck + " and have a " + car);
+}
+
+check_alert2(1200, "Corvette");
+
+check_alert2();
+
+function check_alert3(pcheck, car) {
+    if (pcheck && car) window.alert("You make $" + pcheck + " and have a " + car);
+    else window.alert("My arguments are missing!");
+}
+
+check_alert3();
