c8f3915e5cab3d441ca836caf784a7cfe2929ebc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-31 19:56:46 +0800

    Committed 2021/07/31

--- Chapter4 - Functions/Other ways to define functions - the function expression 2.js
+++ Chapter4 - Functions/Other ways to define functions - the function expression 2.js
@@ -1,5 +1,5 @@
 var send_alert = function (text_part1, text_part2) {
-    window.alert(text_part1, text_part2);
+    window.alert(text_part1 + text_part2);
 };
 
 send_alert("This is ", "Working!");
