cbfda09131cb031be525d8d491d20d4cafa6190b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-14 19:34:56 +0800

    Committed 2021/08/14

--- PHP and MySQL Web Development/Chapter1 - PHP Crash Course/my_processorder3.php
+++ PHP and MySQL Web Development/Chapter1 - PHP Crash Course/my_processorder3.php
@@ -1,8 +1,11 @@
-$tire_quantity = $_POST["tireqty"]
-
 Thank you for the purchase!
 
 <?php
+// This code creates three new variables-$tire_quantity, $oil_quantity, $spark_quantity- and set them
+// to contain the data sent via the POST method from the form.
+$tire_quantity = $_POST["tireqty"];
+$oil_quantity = $_POST["oilqty"];
+$spark_quantity = $_POST["sparkqty"];
 
 echo "<p>Order processed at </p>".date("Y-m-d H:i:s");
 ?>
