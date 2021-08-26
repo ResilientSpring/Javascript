7e8d552b9235e3001d7fc91910046555a1dd26f2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-26 12:32:04 +0800

    Committed 2021/08/26

--- PHP and MySQL Web Development/Chapter1 - PHP Crash Course/String Concatenation/my_order_form2.php
+++ PHP and MySQL Web Development/Chapter1 - PHP Crash Course/String Concatenation/my_order_form2.php
@@ -6,7 +6,7 @@ $tire_quantity = $_POST["tireqty"];
 $oil_quantity = $_POST["oilqty"];
 $spark_quantity = $_POST["sparkqty"];
 
-echo "<p>Order processed at </p>".date("Y-m-d H:i:s")."<br/>";
+echo "<p>Order processed at ".date("Y-m-d H:i:s")."<br/></p>";
 
 echo "Your order content is as follows: <br/>";
 
