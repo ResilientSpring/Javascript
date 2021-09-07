bb4a6fc3f1645801d027d540cdfb4ee5b343fb50
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-07 13:11:46 +0800

    Committed 2021/09/07

---
+++ Chapter4 - Functions/pr4_2.js
@@ -0,0 +1,6 @@
+function display_HTML() {
+    let myheading = <h1>Hello, World!</h1>;
+}
+
+let mytext = <p>While it is nice to know you world,
+    there are only some things that I am comfortable sharing in a global context.</p>
