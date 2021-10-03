5c755c72dfba6e4de732b39b3044f9aad1be2596
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-03 22:03:33 +0800

    Committed 2021/10/03

--- JavaScript a Beginner's Guide/Chapter5 - JavaScript Operators/prjs5_1.js
+++ JavaScript a Beginner's Guide/Chapter5 - JavaScript Operators/prjs5_1.js
@@ -1,13 +1,23 @@
 let paycheck = 2000;
 
+document.writeln(paycheck);
+
 paycheck *= 2;
+
+document.writeln(paycheck);
 
 paycheck -= 500;
 
+document.writeln(paycheck);
+
 paycheck -= 3500;
+
+document.writeln(paycheck);
 
 paycheck += 500;
 
+document.writeln(paycheck);
+
 paycheck = paycheck - 50;
 
-
+document.writeln(paycheck);
