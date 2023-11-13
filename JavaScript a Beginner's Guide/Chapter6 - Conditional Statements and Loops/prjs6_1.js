023871c8243efe21ced3d4e833e2c33187c1088f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-13 21:59:32 +0800

    Committed on or around 2023/11/13

--- JavaScript a Beginner's Guide/Chapter6 - Conditional Statements and Loops/prjs6_1.js
+++ JavaScript a Beginner's Guide/Chapter6 - Conditional Statements and Loops/prjs6_1.js
@@ -1,5 +1,13 @@
 let msg = "";
 var city = window.prompt("What is the name of your city?", "Kaohsiung City");
 
+if ((city == null) || (city === ""))
+    msg = "No city entered. Cannot determine delivery availability";
+else
+    switch (city) {
 
+        case "John\'stown":
+            msg = "We can have items delivered to you in 3 days.";
+
+    }
 
