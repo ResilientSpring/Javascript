372638f4d28bcddcb34f0e3243e2aecc134b0297
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-13 22:03:13 +0800

    Committed on or around 2023/11/13

--- JavaScript a Beginner's Guide/Chapter6 - Conditional Statements and Loops/prjs6_1.js
+++ JavaScript a Beginner's Guide/Chapter6 - Conditional Statements and Loops/prjs6_1.js
@@ -8,6 +8,17 @@ else
 
         case "John\'stown":
             msg = "We can have items delivered to you in 3 days.";
-
+            break;
+        case "Donville":
+            msg = "We can have items delivered to you in 1 week.";
+            break;
+        case "Danieltown":
+            msg = "We can have items delivered to you in 2 weeks."
+            break;
+        case "Martyville":
+            msg = "We can have items delivered to you in 2 weeks.";
+            break;
+        default:
+            msg = "Sorry! We do not deliver to your city yet.";
     }
 
