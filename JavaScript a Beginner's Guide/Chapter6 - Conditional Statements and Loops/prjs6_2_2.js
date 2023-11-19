f7be0805e50c57328a814502ad79d4e5a43ff9ae
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-19 23:46:20 +0800

    Committed on or around 2023/11/19

--- JavaScript a Beginner's Guide/Chapter6 - Conditional Statements and Loops/prjs6_2_2.js
+++ JavaScript a Beginner's Guide/Chapter6 - Conditional Statements and Loops/prjs6_2_2.js
@@ -38,10 +38,7 @@ if (items > 0){
 
     document.write(
 
-        `<p>Print this out, write your item numbers below.</p>`
-
-        "<p>Print this out, write your item numbers below. " +
-        "and mail it to us to order.</p>"
+        `<p>Print this out, write your item numbers below, and mail it to us to order.</p>`
 
     )
 
