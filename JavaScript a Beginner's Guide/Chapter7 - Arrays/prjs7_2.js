6666e87c61661f341ba207e35a9ce146d524ffc4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-24 23:46:43 +0800

    Committed on or around 2024/04/24

---
+++ JavaScript a Beginner's Guide/Chapter7 - Arrays/prjs7_2.js
@@ -0,0 +1,7 @@
+let computer_parts = [
+
+    ["Monitors", "LCD Screens", "vibrant Colors"],
+    ["Motherboards", "Fast"],
+    ["Chips", "Pentium", "Very Fast"],
+    ["Hard Drives", "100-500 GB", "Fast Reading"]
+]
