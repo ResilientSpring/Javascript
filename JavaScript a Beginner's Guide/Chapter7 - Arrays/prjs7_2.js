f272297819ed518e9df294113cf28a470bdea2e7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-24 23:49:05 +0800

    Committed on or around 2024/04/24

--- JavaScript a Beginner's Guide/Chapter7 - Arrays/prjs7_2.js
+++ JavaScript a Beginner's Guide/Chapter7 - Arrays/prjs7_2.js
@@ -3,5 +3,8 @@ let computer_parts = [
     ["Monitors", "LCD Screens", "vibrant Colors"],
     ["Motherboards", "Fast"],
     ["Chips", "Pentium", "Very Fast"],
-    ["Hard Drives", "100-500 GB", "Fast Reading"]
+    ["Hard Drives", "100-500 GB", "Fast Reading"],
+    ["DVD-ROMs", "Burn CDs", "Burn DVDs", "Listen to both!"],
+    ["Cases", "All Sizes", "Choice of Colors"],
+    ["Power Supplies", "We can get one for any computer!"]
 ]
