ae621128dea7d6ffe954a1544f5bb35692cc9cf9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-24 23:52:19 +0800

    Committed on or around 2024/04/24

--- JavaScript a Beginner's Guide/Chapter7 - Arrays/prjs7_2.js
+++ JavaScript a Beginner's Guide/Chapter7 - Arrays/prjs7_2.js
@@ -8,3 +8,20 @@ let computer_parts = [
     ["Cases", "All Sizes", "Choice of Colors"],
     ["Power Supplies", "We can get one for any computer!"]
 ]
+
+for (let i = 0; i < computer_parts.length; i++){
+
+    for (let j = 0; j < computer_parts[i].length; j++){
+
+        if (j === 0){
+            document.write(computer_parts[i][j] + ": ");
+        } else if (j === computer_parts[i].length - 1){
+            document.write(computer_parts[i][j] + "<br>");
+        } else {
+            document.write(computer_parts[i][j] + ", ");
+        }
+
+    }
+
+}
+
